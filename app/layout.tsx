4c3eb31dd3d6b5fc822de39bfa734a9bebfeb8ef
ujjwalrraj <ujjwalrraj@gmail.com>
2024-08-15 14:32:07 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import {
+  ClerkProvider
+} from '@clerk/nextjs'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  </ClerkProvider>
+  );
+}
